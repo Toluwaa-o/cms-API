11560b528604795161faaffbdd3110668759c6ed
Toluwalashe <asheogunleye02@gmail.com>
2023-05-19 04:43:48 +0000

    filter tweak

--- Controllers/userControllers.js
+++ Controllers/userControllers.js
@@ -16,14 +16,14 @@ const getAllCivillians = async (req, res) => {
 }
 
 const getAllOfficers = async (req, res) => {
-    const { firstName } = req.query
+    const { search } = req.query
 
     const page = Number(req.query.page) || 1
     const limit = 10
     const skip = (page - 1) * limit
 
     let queryObject = {userType: 'officer', verified: true}
-    if(firstName) queryObject.firstName = { $regex: firstName, $options: 'i' }
+    if(firstName) queryObject.firstName = { $regex: search, $options: 'i' }
 
     const users = await User.find(queryObject).select('-password').skip(skip).limit(limit)
 
