fddd0d559ddd25753c8a383be960d38e5ee60f68
Toluwalashe <asheogunleye02@gmail.com>
2023-06-05 01:25:42 +0000

    minor tweak

--- Routes/reportsRoutes.js
+++ Routes/reportsRoutes.js
@@ -8,6 +8,6 @@ router.route('/').get(getAllReports).post(createReport)
 router.route('/upload-file').post(uploadFile)
 router.route('/stats').get(authorizeUser('admin', 'officer'), getStats)
 router.route('/:id').get(getReport).patch(updateReport).delete(authorizeUser('admin'), deleteReport)
-router.route('/:id/response').patch(updateReponse)
+router.route('/:id/response').patch(authorizeUser('admin'), updateReponse)
 
 module.exports = router
